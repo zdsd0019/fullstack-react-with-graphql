c0a3b416a1b675a14064a5515e31e5c65e1ce997
kumail <zdsd0019@gmail.com>
2020-09-22 18:38:42 -0400

    added routing with react-router-dom

--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './components/App';
-import Applloclient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
+import "./index.scss";
 
-const client = new Applloclient({
+import App from "./components/App";
+import Signin from "./components/Auth/Signin";
+import Signup from "./components/Auth/Signup";
+
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+const client = new ApolloClient({
   uri: "http://localhost:4444/graphql"
-})
+});
 
-ReactDOM.render(
-<ApolloProvider client={ client }>
-    <App />
-  </ApolloProvider>,
-  document.getElementById('root')
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route path="/" exact component={App} />
+      <Route path="/signin" component={Signin} />
+      <Route path="/signup" component={Signup} />
+      <Redirect to="/" />
+    </Switch>
+  </Router>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Root />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
