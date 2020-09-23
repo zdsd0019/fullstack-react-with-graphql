621e98692fa3064396fae1f1e1043133d08c8cb9
kumail <zdsd0019@gmail.com>
2020-09-22 23:51:25 -0400

    manage input state in signup form

--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -1,15 +1,31 @@
 import React from 'react';
 
 class Signup extends React.Component {
+
+    state = {
+        username: "",
+        email: "",
+        password:"",
+        passwordConfirmation:""
+    }
+
+    handleChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        })
+    }
+
     render() {
+        const { username, email, password, passwordConfirmation} = this.state
         return (
             <div className="App">
                 <h2 className="App">Signup</h2>   
                 <form className="form">
-                    <input type="text" name="username" placeholder="username" />
-                    <input type="email" name="email" placeholder="Email Address" />
-                    <input type="password" name="password" placeholder="password" />
-                    <input type="password" name="passwordConfirmation" placeholder="Confirm Password" />
+                    <input type="text" name="username" placeholder="username" value={ username } onChange={ this.handleChange } />
+                    <input type="email" name="email" placeholder="Email Address" value={ email } onChange={ this.handleChange } />
+                    <input type="password" name="password" placeholder="password" value={ password }  onChange={ this.handleChange } />
+                    <input type="password" name="passwordConfirmation" placeholder="Confirm Password" value={ passwordConfirmation }  onChange={ this.handleChange } />
                     <button type="submit" className="button-primary">Submit</button>
                 </form>
 
